52f1905f574eb1b5b1cca79ccc50325d7af5abda
diego.fernandez <diego.fernandez@106-10.info2>
2024-09-20 14:22:00 +0200

    commit5

---
+++ src/exercise5/Exercise5.java
@@ -0,0 +1,10 @@
+package exercise5;
+
+public class Exercise5 {
+public static void main(String[] args) {
+System.out.println("Name: Diego");
+System.out.println("Adress: Sevilla");
+System.out.println("Telephone Number: 601447224");
+}
+}
+
